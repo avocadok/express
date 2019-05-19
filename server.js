0bbbc6803f058f012727240d99066faead2e2a72
Katarzyna Watroba <kw01826@rmn.com>
2019-05-19 16:31:47 +0100

    working client-server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+	res.send('OK')
+})
+
+
+
+
+app.listen(port, () => {
+	console.log(`Express running → PORT ${port}`);
+});
